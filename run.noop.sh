04290390d52949a6d5f0f5b1214f3f738b338cdd
cmuramoto <cleber.muramoto@gmail.com>
2022-02-08 12:55:26 -0300

    refac: unify run scripts

--- run.noop.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-/opt/java/jdk-19/bin/java \
--XX:+UnlockExperimentalVMOptions -XX:+UseEpsilonGC -XX:+AlwaysPreTouch \
--Xms64m -Xmx64m -XX:MaxDirectMemorySize=2g \
---enable-native-access=ALL-UNNAMED --add-modules jdk.incubator.foreign \
--Djava.library.path=native -cp bin test.EstimateCallOverhead
